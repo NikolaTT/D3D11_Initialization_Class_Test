d4556ac5cd35be1bcb63d31b43d59701abae558b
Nagazaki <nekavsitam@gmail.com>
2014-04-11 16:54:17 +0300

    Commit for the sake of rollback.

---
+++ D3D11_Initialization_Class_Test/BasicLevel.cpp
@@ -0,0 +1,27 @@
+#include "BasicLevel.h"
+
+BasicLevel::BasicLevel(std::string fileName) : levelFileName(fileName){
+
+	GeometryLoader levelGeometryLoader;
+	levelGeometryLoader.LoadGeometry(levelFileName, levelGeometryVector);
+
+	for (int i = 0; i < levelGeometryVector.size(); i++){
+
+		for (int j = 0; j < levelGeometryVector[i].vertices.size(); j++){
+			
+			vertices.push_back(levelGeometryVector[i].vertices[j]);
+
+		}
+	}
+
+}
+
+void BasicLevel::setVertices(){
+
+	for (int i = 0; i < vertices.size(); i++){
+
+		mTDevice->vertices[i] = vertices[i];
+
+	}
+
+}
